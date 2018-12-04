f46b96a58b1e886ffb2714e29b377ec315f854e1
Unknown <simon@imagerie.ca>
2018-12-04 17:24:22 -0500

    Small fix to models

--- Models.php
+++ Models.php
@@ -1,35 +1,39 @@
 <?php
-
+// SetUp Models
 namespace Balise\Bridge;
-
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Database\Eloquent\Model as Model;
 
-include_once(ABSPATH.'wp-admin/includes/plugin.php');
+class Model extends Illuminate\Database\Eloquent\Model {
+    static function _registerModels() {
+        include_once(ABSPATH.'wp-admin/includes/plugin.php');
 
-$capsule = new Capsule;
-$selfData = get_plugin_data(dirname(__DIR__).'/config.php');
+        $capsule = new Capsule;
+        $selfData = get_plugin_data(dirname(__DIR__).'/config.php');
 
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => DB_HOST,
-    'database'  => DB_NAME,
-    'username'  => DB_USER,
-    'password'  => DB_PASSWORD,
-    'charset'   => DB_CHARSET,
-    'collation' => ((DB_COLLATE) ? DB_COLLATE : NULL),
-    'prefix'    => $wpdb->prefix.$selfData["TextDomain"]."_"
-]);
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => DB_HOST,
+            'database'  => DB_NAME,
+            'username'  => DB_USER,
+            'password'  => DB_PASSWORD,
+            'charset'   => DB_CHARSET,
+            'collation' => ((DB_COLLATE) ? DB_COLLATE : NULL),
+            'prefix'    => $wpdb->prefix.$selfData["TextDomain"]."_"
+        ]);
 
-// Make this Capsule instance available globally via static methods
-$capsule->setAsGlobal();
+        // Make this Capsule instance available globally via static methods
+        $capsule->setAsGlobal();
 
-// Setup the Eloquent ORM
-$capsule->bootEloquent();
+        // Setup the Eloquent ORM
+        $capsule->bootEloquent();
 
-//Load all models
-$files=glob(__DIR__."/app/models/*.php");
-foreach ($files as $file) {
-    require_once($file);
+        //Load all models
+        $files=glob(__DIR__."/app/models/*.php");
+        foreach ($files as $file) {
+            require_once($file);
+        }
+        do_action( 'activate_plugin', dirname(__DIR__).'/config.php' );
+
+    }
 }
-do_action( 'activate_plugin', dirname(__DIR__).'/config.php' );
+Model::_registerModels();
