133fd8f8ec4862a70a03cccec5d2786566169416
Simon Mongeau <simon@imagerie.ca>
2019-01-08 11:47:19 -0500

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,7 +1,12 @@
 <?php
 // SetUp Controller
 namespace Balise\Bridge;
+use Windwalker\Renderer\BladeRenderer;
 
 class Controller {
-
+    protected function view() {
+            echo substr(get_called_class(), 0, strrpos(get_called_class(), "\\"));
+            $renderer = new BladeRenderer();
+                
+    }
 }
