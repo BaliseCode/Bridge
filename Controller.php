63fc1aea384b0ee06bf806f3328f39ef8041648f
Simon Mongeau <simon@imagerie.ca>
2019-01-08 12:34:33 -0500

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -4,9 +4,16 @@ namespace Balise\Bridge;
 use Windwalker\Renderer\BladeRenderer;
 
 class Controller {
-    protected function view() {
-            echo substr(get_called_class(), 0, strrpos(get_called_class(), "\\"));
-            $renderer = new BladeRenderer();
+    protected function view($view, $data=array()) {
+        $ns = substr(get_called_class(), 0, strrpos(get_called_class(), "\\"));
+        if (defined ($ns. '\ViewDir') && defined ($ns. '\ViewCache')) {
+           $renderer = new BladeRenderer(constant($ns. '\ViewDir'), array('cache_path' => constant($ns. '\ViewCache')));
+           return $renderer->render($view, $data);
+        } else {
+            throw new \Exception("Please define ".$ns."\ViewDir and  ".$ns."\ViewCache.\n");
+        }
+           
+        
                 
     }
 }
